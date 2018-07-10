d057d150263c66baecbf887216fe18c5958ea7ee
yp-PC\yp <hj0030205@naver.com>
2018-07-10 20:30:48 +0900

    Merge 후 자잘한 오류 수정.

--- feline/src/main/java/com/feline/member/MemberController.java
+++ feline/src/main/java/com/feline/member/MemberController.java
@@ -8,12 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.feline.ccr.CancleModel;
@@ -21,7 +19,7 @@ import com.feline.ccr.ChangeModel;
 import com.feline.ccr.RefundModel;
 import com.feline.goods.GoodsModel;
 import com.feline.order.OrderModel;
-import com.feline.order.OrderService;
+
 
 @Controller
 @RequestMapping("/member")
